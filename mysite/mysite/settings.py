05bed9baa7d8f1bae57a45894ef66e3569cce25a
GDIINDIA\mansharma <mansharma@birchstreet.net>
2023-01-03 14:28:37 +0530

    1st branch commit on 3rd Jan

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['D:\Python project\mysite\Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
