78b748114d4371607c3667d1757a200816e64d2d
GDIINDIA\mansharma <mansharma@birchstreet.net>
2023-01-03 21:32:54 +0530

    last commit

--- mysite/Caloriecounter/models.py
+++ mysite/Caloriecounter/models.py
@@ -4,5 +4,8 @@ class Person(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     address = models.TextField()
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 # Create your models here.
